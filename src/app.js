c99dd7b4b6b3e2bd4aebc5e1fcb02ddf07119e2c
Rajat <rpandey@mdsol.com>
2019-11-20 22:06:52 +0530

    We server with static HTML

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+const helpPage = path.join(__dirname, './pages/help-page.html') //path join takes two arguments
+app.get('/help', (req, res) => {
+    res.sendFile(helpPage) // For sending file in response 
+})
 
-app.get('', (req, res) => {
+app.get('', (req, res) => { // app.get('', (res, req) => {})
     res.send('Hello Express!!')
+})
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'Cloudy with 50 degree temperature',
+        location: 'Texas'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.send({
+        name: 'Rajat',
+        designation: 'Full Stack Engineer'
+    })
 })
 
 app.listen(3000, () => {
